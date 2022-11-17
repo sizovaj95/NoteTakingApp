380dfc18a887fffe6c4ca6ca43691491900d3df5
Julia <54853811+sizovaj95@users.noreply.github.com>
2022-11-17 08:41:09 +0000

    Finished mocking

--- UnitTests/NoteTakingTests.cs
+++ UnitTests/NoteTakingTests.cs
@@ -12,21 +12,40 @@ namespace UnitTests
         [TestMethod]
         public void GetNotes1()
         {
-            List<string> expectedNotes = new List<string>()
-            {
-                "Buy milk",
-                "Pay bills",
-                "Feed capybara"
-            };
+            List<NoteInfo> expectedNotes = new List<NoteInfo>();
+            expectedNotes.Add(new NoteInfo { Note = "Buy milk", Date = "Thu, 17 November 2022", Time = "07:14" });
+            expectedNotes.Add(new NoteInfo { Note = "Pay bills", Date = "Thu, 17 November 2022", Time = "07:15" });
+            expectedNotes.Add(new NoteInfo { Note = "Feed capybara", Date = "Thu, 17 November 2022", Time = "07:16" });
+
+            DateTime dateTimeNow = new DateTime(2022, 11, 17, 7, 14, 50);
+            DateTime anyDate;
+
             var consoleManagerMock = new Mock<IConsoleManager>();
             var timeManagerMock = new Mock<ITimeManager>();
-            consoleManagerMock.Setup(p => p.ReadLine()).Returns("Buy milk");
-            consoleManagerMock.Setup(p => p.WriteLine("Buy milk"));
-            consoleManagerMock.Setup(p => p.ReadKey(true)).Returns(ConsoleKey.Enter);
+            
+            consoleManagerMock.SetupSequence(p => p.WriteLine(It.IsAny<string>()));
+            consoleManagerMock.SetupSequence(p => p.ReadLine())
+                .Returns("Buy milk")
+                .Returns("Pay bills")
+                .Returns("Feed capybara");
+            consoleManagerMock.SetupSequence(p => p.ReadKey(true))
+                .Returns(ConsoleKey.Enter)
+                .Returns(ConsoleKey.Enter)
+                .Returns(ConsoleKey.Escape);
+
+            timeManagerMock.SetupSequence(p => p.DateTimeNow())
+                .Returns(dateTimeNow)
+                .Returns(dateTimeNow.AddMinutes(1))
+                .Returns(dateTimeNow.AddMinutes(2));
+            //timeManagerMock.Setup(p => p.DateToString(It.IsAny<DateTime>())).Returns(anyDate.ToString("ddd, dd MMMM yyyy"));
+            timeManagerMock.Setup(p => p.DateToString(It.IsAny<DateTime>())).Returns((DateTime x) => x.ToString("ddd, dd MMMM yyyy"));
+            timeManagerMock.Setup(p => p.TimeToString(It.IsAny<DateTime>())).Returns((DateTime x) => x.ToString("HH:mm"));
+            //timeManagerMock.Setup(p => p.TimeToString(It.IsAny<DateTime>())).Returns(dateTimeNow.ToString("HH:mm"));
 
             NoteManager noteManager = new NoteManager(consoleManagerMock.Object, timeManagerMock.Object);
             List<NoteInfo> actualNotes = noteManager.GetNotes();
-            Assert.AreEqual(expectedNotes.Count, actualNotes.Count);
+            CollectionAssert.AreEquivalent(expectedNotes, actualNotes);
+            
         }
     }
-}
+}		
